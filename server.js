4f3f86658e626651c26b8b263b76e2ac2bcfd168
Kimberly Ames <Kimberly Ames>
2016-11-21 13:31:29 -0700

    updating routes and templating system using handlebars

--- server.js
+++ server.js
@@ -7,6 +7,22 @@ var server = express ();
 // Set the port that our server will run on.
 var port = 3000;
 
+// Configure the render engine handlebars.
+var handlebars = require ('express-handlebars');
+server.engine ('.hbs', handlebars({
+    layoutsDir: 'templates',                // The directory of layout files.
+    //partialsDir: '/templates/partials',      // The directory for partial files.
+    defaultLayout: 'index',                 // The base/main template to always load.
+    extname: '.hbs'                         // The file extension to use.
+}));
+
+// Set the default directory for express to use for
+// the handlebar templates.
+server.set ('views', __dirname + '\\templates\\partials');
+
+// Set the render engine for our server.
+server.set ('view engine', '.hbs');
+
 //Launch the server app.
 server.listen (port, function (error){
     // Check to see if the server was unable to startup
@@ -27,6 +43,16 @@ server.get ('/', function (request, response){
 
     // The response object is used to send responses back to the user
     // who made the request.
-    response.send (<h2>'Hello World!'</h2>);
+
+    //response.send ('<h2>Hello World!</h2>');
+
+    // Have express render out the string/text markup response
+    // that will go to the client.
+    response.render ('home');
+
 });
-;
+
+// About route
+server.get ('/about', function (request, response) {
+    response.render ('about');
+});
